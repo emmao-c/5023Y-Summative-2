2516296a2423accad3611892b0a2767aaeb02f52
Emma OConnor <zzq21wcu@uea.ac.uk>
2024-04-29 16:03:16 +0000

    box and whisker plot to investigate abundance

--- scripts/Emma.R
+++ scripts/Emma.R
@@ -40,46 +40,44 @@ probiotic$group <- as.factor(probiotic$group)
 probiotic$time <- as.factor(probiotic$time)
 
 
-#Monovariate analysis 
-
-
-
-
-
-
-
-
 prob_sort <- probiotic %>%
   group_by(subject,gender,group)%>%
   summarise(abundance_before =ruminococcus_gnavus_abund[time ==1],
             abundance_after =ruminococcus_gnavus_abund[time==2])
 
-
-prob_diff %>% 
-  drop_na(gender) %>% 
-  ggplot(aes(x = abund_diff,
-             y = group,
-             colour = gender))+
-  geom_point(position = position_jitterdodge()) +
-  coord_flip()
-
-prob_sort %>%
-abundance_difference <- (abundance_after - abundance_before)
-
-# extracting column 1 into a 
-# variable called a 
-a=prob_sort$abundance_after 
-
-# extracting column 2 into a
-# variable called b
-b=prob_sort$abundance_before 
-
-# printing the difference between 
-# two columns 
-print(a-b) 
-
 prob_diff <- prob_sort %>%
   select(subject, gender, group,abundance_after,abundance_before) %>%
   mutate(abund_diff = abundance_after - abundance_before )
+
+#Monovariate analysis 
+
+
+ggplot(data = prob_diff, aes(x = group, y = abundance_after)) +
+  geom_boxplot(aes(fill = gender),
+               alpha = 0.7, 
+               width = 0.5, # change width of boxplot
+               show.legend = FALSE)
+
+ggplot(data = prob_diff, aes(x = group, y = abundance_before)) +
+  geom_boxplot(aes(fill = gender),
+               alpha = 0.7, 
+               width = 0.5, # change width of boxplot
+               show.legend = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
