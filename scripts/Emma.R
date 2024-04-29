03bc72b8a60652604034fb0726ec5c8d63e660e6
Emma OConnor <zzq21wcu@uea.ac.uk>
2024-04-29 13:32:13 +0000

    Begining monovariate analysis on dataset

--- scripts/Emma.R
+++ scripts/Emma.R
@@ -40,16 +40,46 @@ probiotic$group <- as.factor(probiotic$group)
 probiotic$time <- as.factor(probiotic$time)
 
 
+#Monovariate analysis 
+
+
+
+
+
+
+
+
 prob_sort <- probiotic %>%
   group_by(subject,gender,group)%>%
   summarise(abundance_before =ruminococcus_gnavus_abund[time ==1],
             abundance_after =ruminococcus_gnavus_abund[time==2])
 
 
-library(ggdist)
-prob_sort %>% 
+prob_diff %>% 
   drop_na(gender) %>% 
-  ggplot(aes(y = abundance_before,abundance_after
-             x = gender,
-             fill = gender))+
-  stat_halfeye()
+  ggplot(aes(x = abund_diff,
+             y = group,
+             colour = gender))+
+  geom_point(position = position_jitterdodge()) +
+  coord_flip()
+
+prob_sort %>%
+abundance_difference <- (abundance_after - abundance_before)
+
+# extracting column 1 into a 
+# variable called a 
+a=prob_sort$abundance_after 
+
+# extracting column 2 into a
+# variable called b
+b=prob_sort$abundance_before 
+
+# printing the difference between 
+# two columns 
+print(a-b) 
+
+prob_diff <- prob_sort %>%
+  select(subject, gender, group,abundance_after,abundance_before) %>%
+  mutate(abund_diff = abundance_after - abundance_before )
+
+
